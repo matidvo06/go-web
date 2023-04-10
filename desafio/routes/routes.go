6777b29cb464c4949bd6ecd1ceddafed9fac26f0
Matias Dvorsky <matias.dvorsky@mercadolibre.com>
2023-04-10 11:44:34 -0300

    Create routes.go

---
+++ desafio/routes/routes.go
@@ -0,0 +1,72 @@
+package routes
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"strconv"
+	
+	"github.com/bootcamp-go/desafio-go-web/internal/tickets"
+)
+
+type Response struct {
+	Success bool `json:"success"`
+	Data interface{} `json:"data"`
+	Error string `json:"error"`
+}
+
+func handleGetByCountry(repo tickets.Repository) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		ctx := context.Background()
+		
+		dest := r.URL.Query().Get("dest")
+		
+		if dest == "" {
+			http.Error(w, "Missing destination parameter", http.StatusBadRequest)
+			return
+		}
+		
+		ticketList, err := repo.GetTicketByDestination(ctx, dest)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		
+		response := Response {
+			Success: true,
+			Data: ticketList,
+			Error: "",
+		}
+		
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+func handleGetAverage(repo tickets.Repository) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		ctx := context.Background()
+		
+		dest := r.URL.Query().Get("dest")
+		
+		if dest == "" {
+			http.Error(w, "Missing destination parameter", http.StatusBadRequest)
+			return
+		}
+		
+		service := tickets.NewService(repo)
+		
+		avg, err := service.AverageDestination(ctx, dest)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		
+		response := Response {
+			Success: true,
+			Data: avg,
+			Error: "",
+		}
+		json.NewEncoder(w).Encode(response)
+	}
+}
+		
